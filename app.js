72e41029b834abf2641befd3452fa9c1fa8347c7
Danil <danil.kersontsev@khk.ee>
2023-04-19 12:28:48 +0300

    Added feature to save city name to LS and load it from LS

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const weather = new Weather("Tartu")
+const ls = new LS()
+const loadCity = ls.getCity()
+const weather = new Weather(loadCity)
 const ui = new UI()
 
-
 const form = document.querySelector("#change-city")
+
 form.addEventListener("submit", changeWeather)
+
 
 function changeWeather(e){
     const city = document.querySelector("#city-name").value
+    ls.addCity(city)
     weather.changeCity(city)
     getWeather()
     document.querySelector(("#city-name")).value = ""
     e.preventDefault()
 }
-
 
 function getWeather(){
     weather.getWeather()
@@ -22,5 +25,7 @@ function getWeather(){
         })
         .catch(error => console.log(error))
 }
+
+
 
 getWeather()
