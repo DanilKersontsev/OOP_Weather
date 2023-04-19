4e2e863bb36c0b5a0e628413acf609e553f56080
Danil <danil.kersontsev@khk.ee>
2023-04-19 11:52:52 +0300

    GetWeatherData function added

---
+++ Weather.js
@@ -0,0 +1,12 @@
+class Weather{
+    constructor(city){
+        this.city = city
+        this.key = "c827e4dd7e017064b364d6b506bf2217"
+    }
+    async getWeather(){
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responseData = await response.json()
+        return responseData
+    }
+
+}
